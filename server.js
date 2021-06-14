ac32fc86daccb9ad8150cc34d5a1000965c9bfd7
KalebeLopes <calbkalebe@gmail.com>
2021-06-14 12:58:27 -0300

    feat: set up passport

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const passport = require('passport');
+const session = require('express-session');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
@@ -15,6 +17,16 @@ app.set('view engine', 'pug')
 app.route('/').get((req, res) => {
   res.render(process.cwd() + '/views/pug', {title: 'Hello', message: 'Please login'});
 });
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
